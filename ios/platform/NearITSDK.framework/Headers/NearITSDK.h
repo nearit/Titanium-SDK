bc1e5e56975e2b8f166a321cbbaaa6fa5c4d9ae0
Federico Boschini <federico@nearit.com>
2018-09-14 11:21:09 +0200

    chore(project): add NearITSDK framework in platform dir

---
+++ ios/platform/NearITSDK.framework/Headers/NearITSDK.h
@@ -0,0 +1,38 @@
+//
+//  NearITSDK.h
+//  NearITSDK
+//
+//  Created by Francesco Leoni on 14/03/17.
+//  Copyright © 2017 NearIT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import <NITManager.h>
+#import <NITContent.h>
+#import <NITImage.h>
+#import <NITResource.h>
+#import <NITRecipe.h>
+#import <NITCoupon.h>
+#import <NITClaim.h>
+#import <NITFeedback.h>
+#import <NITFeedbackEvent.h>
+#import <NITCustomJSON.h>
+#import <NITSimpleNotification.h>
+#import <NITEvent.h>
+#import <NITAudio.h>
+#import <NITUpload.h>
+#import <NITLogger.h>
+#import <NITLog.h>
+#import <NITReactionBundle.h>
+#import <NITContentLink.h>
+
+//! Project version number for NearITSDK.
+FOUNDATION_EXPORT double NearITSDKVersionNumber;
+
+//! Project version string for NearITSDK.
+FOUNDATION_EXPORT const unsigned char NearITSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NearITSDK/PublicHeader.h>
+
+
